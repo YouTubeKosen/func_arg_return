30980875aa2c7e211411f08858e0aae2e99e9c20
YouTubeKosen <private.kosen@gmail.com>
2023-03-11 11:17:25 +0900

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdint.h>
+
+void callByRef(uint32_t & num)
+{
+    num = 80;
+}
+
+void callByValue(uint32_t num)
+{
+    num = 80;
+}
+
+int main(void) {
+    uint32_t num;
+    num = 50;
+    std::cout << num << "\n";
+
+    callByValue(num);
+    std::cout << num << "\n";
+
+    callByRef(num);
+    std::cout << num << "\n";
+
+    return 0;
+}
