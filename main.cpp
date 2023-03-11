2b11a1bc868fcaa6516cda0f608ce326bcd57cda
YouTubeKosen <private.kosen@gmail.com>
2023-03-11 19:06:45 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <stdint.h>
 
-uint32_t callStack(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4, uint32_t arg5, uint32_t arg6, uint32_t arg7)
+uint64_t callStack(uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6, uint64_t arg7)
 {
-    return 0;
+    return arg0 + arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7;
 }
 
 int main(void) {
-    uint32_t num;
+    uint64_t num;
     num = 50;
 
     callStack(num, num, num, num, num, num, num, num);
