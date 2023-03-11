b91ef90c64d6d1c2a47f4db382848149c079126d
YouTubeKosen <private.kosen@gmail.com>
2023-03-11 17:45:40 +0900

    add get_object_id

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-def set_val(a, b):
-    a = b
-    return a
+def get_object_id(a):
+    return id(a)
 
-hoge = 1
-fuga = 2
-hogefuga = set_val(hoge, fuga)
-
-print(f'hoge = {hoge}')
-print(f'fuga = {fuga}')
-print(f'hogefuga = {hogefuga}')
-
-# def set_val_list(a, b):
-#     a[0] = b
-#     return a
-
-# hoge = [1]
-# fuga = 2
-# hogefuga = set_val_list(hoge, fuga)
-
-# print(f'hoge = {hoge}')
-# print(f'fuga = {fuga}')
-# print(f'hogefuga = {hogefuga}')
+var = "hello world!"
+print(id(var))
+print(get_object_id(var))
